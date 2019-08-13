3930e8e1d97c2e1e9e67a6a91fb290bfb22844d3
MattGThomas <mgthomas0616@gmail.com>
2019-08-12 22:47:52 -0500

    added logger MW, ini commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -8,8 +10,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
+// LOGGER FUNCTION
 function logger(req, res, next) {
-
+  console.log(`a ${req.method} request was made to ${req.url} @ ${new Date()}`)
+  next()
 };
 
 module.exports = server;
