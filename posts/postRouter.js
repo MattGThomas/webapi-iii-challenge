6d920f44782a9cca900587722d39ee1c5852a0ea
MattGThomas <mgthomas0616@gmail.com>
2019-08-15 08:31:46 -0500

    finished postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,71 @@
-const express = 'express';
+const express = require('express');
 
+const postDB = require('./postDb.js')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    postDB.get()
+    .then(post => {
+        res.status(201).json(post)
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'there was an error retrieving the posts'
+        })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    res.status(200).json(req.post)
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    const { id } = req.params
+    postDB.remove(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'there was an error deleting the post'
+        })
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, (req, res) => {
+    const postChanges = req.body
+    const { id } = req.params
+    postDB.update(id, postChanges)
+    .then(updatedPost => {
+        res.status(200).json(updatedPost)
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'there was an error updating the post'
+        })
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const { id } = req.params
+    postDB.getById(id)
+    .then(post => {
+        if(post) {
+            req.post = post
+            next()
+        } else {
+            res.status(400).json({
+                message: 'invalid post id'
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'there was an error processing the request'
+        })
+    })
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const userRouter = require('./users/userRouter.js')
-
+const postRouter = require('./posts/postRouter.js')
 const server = express();
 
 const bodyParser = express.json()
@@ -9,6 +9,7 @@ const bodyParser = express.json()
 server.use(bodyParser)
 server.use(logger)
 server.use('/users', userRouter)
+server.use('/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
